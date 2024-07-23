9574f633030d232b3dfbc8f08808632cd11e9d22
Shikhare Shivam <shikhareshivam123@gmail.com>
2024-07-23 12:12:25 +0530

    if else if

---
+++ src/javaDsa/HefShineLogic2.java
@@ -0,0 +1,27 @@
+package javaDsa;
+
+import java.util.Scanner;
+
+public class HefShineLogic2 {
+
+	public static void main(String...args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your amount == ");
+		int amt = sc.nextInt();
+		
+		//test cases 1.50 Mess 2.600 KFC 3.5000 5star
+		
+		if(amt< 100) {
+			System.out.println("Mess");
+		}else if (amt < 200) {
+			System.out.println("Thali");
+		}else if(amt < 500) {
+			System.out.println("McD");
+		}else if(amt < 2000  ) {
+			System.out.println("KFC");
+		}else {
+			System.out.println("5 star");
+		}
+	}
+	
+}
